c02ec366e6d0a705ae3cb24d037d4b503d38557c
ErrorSaid <99440391+ErrorSaid@users.noreply.github.com>
2022-02-24 15:35:25 +0100

    Update Plugin.cs

--- Infection-SCP-096/Plugin.cs
+++ Infection-SCP-096/Plugin.cs
@@ -10,7 +10,7 @@ namespace Infection_SCP_096
         public override string Name { get; } = "infection";
         public override string Author { get; } = "! Said";
         public override Version Version { get; } = new Version(1, 0, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(4, 2, 3);
+        public override Version RequiredExiledVersion { get; } = new Version(4, 2, 5);
 
         public static Config SharedConfig { get; set; }
         public static DateTime lastUsed;
